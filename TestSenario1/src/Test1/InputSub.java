8835bafddc0c126e6fea4a590d1a625fd88a8e11
Naveen <Naveen@LAPTOP-IVL0NE2V>
2024-11-21 00:01:15 +0530

    Initial Commit

--- TestSenario1/src/Test1/InputSub.java
+++ TestSenario1/src/Test1/InputSub.java
@@ -4,11 +4,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class InputSub {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		WebDriver driver = new ChromeDriver();
 		
@@ -22,9 +23,47 @@ public class InputSub {
 	       
 	       btn.click();
 	       
-	       WebDriverWait wait = new WebDriverWait(driver, 10);
-           wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@required]")));
+	      
+	       
+	       WebElement nameField = driver.findElement(By.id("name"));
+	       nameField.sendKeys("John Doe");
+           WebElement emailField = driver.findElement(By.id("inputEmail4"));
+           emailField.sendKeys("john.doe@example.com");
+           WebElement password = driver.findElement(By.name("password"));
+           password.sendKeys("1234567890");
+           WebElement messageField = driver.findElement(By.id("company"));
+           messageField.sendKeys("This is a test message.");
+           WebElement Website = driver.findElement(By.id("websitename"));
+           Website.sendKeys("uysfuygf.com");
+           WebElement country = driver.findElement(By.name("country"));
+           country.click();
+           
+           
 
+           Select s = new Select(country);
+           Thread.sleep(2000);
+         
+          s.selectByVisibleText("United States");
+        
+          WebElement city = driver.findElement(By.id("inputCity"));
+          city.sendKeys("chagallu");
+         
+          WebElement ad1 = driver.findElement(By.id("inputAddress1"));
+          ad1.sendKeys("Nelaturu");
+          WebElement ad2 = driver.findElement(By.id("inputAddress2"));
+          ad2.sendKeys("village");
+          WebElement state = driver.findElement(By.id("inputState"));
+          state.sendKeys("AP");
+          WebElement pincode = driver.findElement(By.name("zip"));
+          pincode.sendKeys("5463746");
+           
+          
+          WebElement btn1 = driver.findElement(By.xpath("(//button[@type='submit'])[2]"));
+	       
+	       btn1.click();
+           
+
+           
 	       
 	       
 	    }
